de8d8fa50740f8b75dd3c48ac19a5017766af5d2
harshwatkar <h.watkar46@gmail.com>
2026-02-19 18:29:37 +0530

    fixed error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/EnvatoHeader";
 import Footer from "@/components/Footer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const inter = Inter({
   variable: "--font-inter",
